87505ab818b5265766c11fa6e6dc3206aea5c655
Mukund Mauji <mauji.mukund@yahoo.ca>
2025-04-17 20:03:56 -0400

    use tabwidth of 2

--- src/main.ts
+++ src/main.ts
@@ -2,38 +2,34 @@ import { Notice, Plugin, TFile } from 'obsidian'
 import { SettingsTab, SettingsInterface, DEFAULT_SETTINGS } from './settings'
 
 export default class OpenMyNotePlugin extends Plugin {
-    settings: SettingsInterface
+  settings: SettingsInterface
 
-    async onload() {
-        await this.loadSettings()
+  async onload() {
+    await this.loadSettings()
 
-        this.addCommand({
-            id: 'open-my-favorite-note',
-            name: 'Open My Favorite Note',
-            callback: async () => {
-                const file = this.app.vault.getAbstractFileByPath(
-                    this.settings.notePath,
-                )
-                if (file && file instanceof TFile) {
-                    this.app.workspace.getLeaf(true).openFile(file)
-                } else {
-                    new Notice('Note not found at: ' + this.settings.notePath)
-                }
-            },
-        })
-
-        this.addSettingTab(new SettingsTab(this.app, this))
-    }
-
-    async loadSettings() {
-        this.settings = Object.assign(
-            {},
-            DEFAULT_SETTINGS,
-            await this.loadData(),
+    this.addCommand({
+      id: 'open-my-favorite-note',
+      name: 'Open My Favorite Note',
+      callback: async () => {
+        const file = this.app.vault.getAbstractFileByPath(
+          this.settings.notePath,
         )
-    }
+        if (file && file instanceof TFile) {
+          this.app.workspace.getLeaf(true).openFile(file)
+        } else {
+          new Notice('Note not found at: ' + this.settings.notePath)
+        }
+      },
+    })
 
-    async saveSettings() {
-        await this.saveData(this.settings)
-    }
+    this.addSettingTab(new SettingsTab(this.app, this))
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -2,40 +2,40 @@ import { App, PluginSettingTab, Setting } from 'obsidian'
 import OpenMyNotePlugin from './main'
 
 export interface SettingsInterface {
-    notePath: string
+  notePath: string
 }
 
 export const DEFAULT_SETTINGS: SettingsInterface = {
-    notePath: 'the note.md',
+  notePath: 'the note.md',
 }
 
 export class SettingsTab extends PluginSettingTab {
-    plugin: OpenMyNotePlugin
+  plugin: OpenMyNotePlugin
 
-    constructor(app: App, plugin: OpenMyNotePlugin) {
-        super(app, plugin)
-        this.plugin = plugin
-    }
+  constructor(app: App, plugin: OpenMyNotePlugin) {
+    super(app, plugin)
+    this.plugin = plugin
+  }
 
-    display(): void {
-        const { containerEl } = this
+  display(): void {
+    const { containerEl } = this
 
-        containerEl.empty()
-        containerEl.createEl('h2', { text: 'Open My Favorite Note Settings' })
+    containerEl.empty()
+    containerEl.createEl('h2', { text: 'Open My Favorite Note Settings' })
 
-        new Setting(containerEl)
-            .setName('Note Path')
-            .setDesc(
-                "Enter the exact path to the note you want to open (e.g., 'Folder/Note.md')",
-            )
-            .addText((text) =>
-                text
-                    .setPlaceholder('MyNotes/FavoriteNote.md')
-                    .setValue(this.plugin.settings.notePath)
-                    .onChange(async (value) => {
-                        this.plugin.settings.notePath = value
-                        await this.plugin.saveSettings()
-                    }),
-            )
-    }
+    new Setting(containerEl)
+      .setName('Note Path')
+      .setDesc(
+        "Enter the exact path to the note you want to open (e.g., 'Folder/Note.md')",
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder('MyNotes/FavoriteNote.md')
+          .setValue(this.plugin.settings.notePath)
+          .onChange(async (value) => {
+            this.plugin.settings.notePath = value
+            await this.plugin.saveSettings()
+          }),
+      )
+  }
 }
